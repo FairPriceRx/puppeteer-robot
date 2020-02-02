d08e8b1388db5015dfb1dd3b9440d27a9279e5c6
Arsen A. Gutsal <a.gutsal@softsky.company>
2020-02-02 17:51:20 +0200

    .editorconfig formatting applied

--- src/app.js
+++ src/app.js
@@ -5,49 +5,49 @@ require('dotenv-flow').config()
 
 const { PayPalRobot } = require('./payPalRobot')
 var App = (function () {
-    function App() {
-				const that = this;
-        console.log('Robot App is running!');
+  function App() {
+		const that = this;
+    console.log('Robot App is running!');
 
-				const server = express()
-				const port = process.env.PUPPETEER_SERVER_PORT || 8080
+		const server = express()
+		const port = process.env.PUPPETEER_SERVER_PORT || 8080
 
-				const botPP = that.botPP = new PayPalRobot({
-						proxyUrl: process.env.PROXY_CFG,
-						headless: false,
-						slowMo: 25,
-						args: [
-								'--force-device-scale-factor=1.0',
-								'--display=:1'
-						]
-				})
-				botPP.init()
+		const botPP = that.botPP = new PayPalRobot({
+			proxyUrl: process.env.PROXY_CFG,
+			headless: false,
+			slowMo: 25,
+			args: [
+				'--force-device-scale-factor=1.0',
+				'--display=:1'
+			]
+		})
+		botPP.init()
+		
+
+		server.get('/', (req, res) => res.send('live'))
+		server.post('/paypal/create_order',(req, res) => {
+			req.on('data', async (order) => {
+				const jsonOrder = JSON.parse(order)
+				console.log(`Sending order to server: ${order}`)
+				// logging in
+				await botPP.login(process.env.PP_LOGIN,
+													process.env.PP_PASSWD)
+
+				await botPP.createOrder(jsonOrder)
+
+				await botPP.currentPage.waitFor(2000); // waiting for order to be actually send
 				
+				//await botPP.logout() // logs out and close browser
+				res.send('OK')
 
-				server.get('/', (req, res) => res.send('live'))
-				server.post('/paypal/create_order',(req, res) => {
-						req.on('data', async (order) => {
-								const jsonOrder = JSON.parse(order)
-								console.log(`Sending order to server: ${order}`)
-								// logging in
-								await botPP.login(process.env.PP_LOGIN,
-																	process.env.PP_PASSWD)
+				// that = botPP = app.botPP;page = that.currentPage;order = require('./order_json.json')
+			})
+		})
 
-								await botPP.createOrder(jsonOrder)
-
-								await botPP.currentPage.waitFor(2000); // waiting for order to be actually send
-								
-								//await botPP.logout() // logs out and close browser
-								res.send('OK')
-
-								// that = botPP = app.botPP;page = that.currentPage;order = require('./order_json.json')
-						})
-				})
-
-				server.listen(port, () => console.log(`Example app listening on port ${port}!`))
-				
-    }
-    return App;
+		server.listen(port, () => console.log(`Example app listening on port ${port}!`))
+		
+  }
+  return App;
 })();
 exports.App = App;
 var app = new App();
